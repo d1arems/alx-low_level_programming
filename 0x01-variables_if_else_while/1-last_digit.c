a5207ce16130893a1c6d592132f21aa624fcee6d
d1arems <d1arems@gmail.com>
2022-09-12 17:04:05 -0700

    one last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-		printf("Last digit of %d is %d and is 0/n", n, n % 10)
+		printf("Last digit of %d is %d and is 0/n", n, n % 10);
 	}
 	else if ((n % 10 < 6) && (n != 0))
 	{
@@ -27,4 +27,3 @@ int main(void)
 	}
 	return (0);
 }
-
