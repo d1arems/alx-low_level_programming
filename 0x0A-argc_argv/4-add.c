1313117959c300911ddc84ebbd7a8ecd82669b2e
d1arems <d1arems@gmail.com>
2022-10-02 07:13:18 -0700

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
  * main - adds numbers
  * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
- *
  * Return: 0 if no errors, else 1
  */
+
 int main(int argc, char *argv[])
 {
 	int a = 0, i, j;
@@ -18,16 +17,16 @@ int main(int argc, char *argv[])
 		for (j = 0; argv[i][j]; j++)
 		{
 			if (isdigit(argv[i][j]) == 0)
+			{
 				puts("Error");
-		return (1);
+				return (1);
+			}
+		}
 	}
-}
-}
-}
-for (i = 1; i < argc; i++)
-{
-	a += atoi(argv[i]);
-}
-printf("%d\n", a);
-return (0);
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
